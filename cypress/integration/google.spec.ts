d96b93caf30a7976c6c47c1b499d504d230ea1d0
Millionhat <38446282+Millionhat@users.noreply.github.com>
2021-07-22 19:09:03 -0500

    eslint setup configuration

---
+++ cypress/integration/google.spec.ts
@@ -0,0 +1,7 @@
+/* eslint-disable no-undef */
+describe("This is my first cypress test", () => {
+  it("should have a title", () => {
+    cy.visit("https://www.google.com/");
+    cy.title().should("eq", "Google");
+  });
+});
